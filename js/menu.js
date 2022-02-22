780bb5b5e6c72f22788e5849e5544fabc58575bf
Angel4EverMore <jangelmore@gmail.com>
2022-02-22 14:56:36 -0600

    se corrige efecto hover en botón scroll to top

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,6 @@
 const d = document,
       w = window,
       $btnScrollToTop = d.getElementById("scroll-to-top-btn")
-      // $mainDiv = d.getElementById("acerca")
-      // $mainHeader = d.querySelector(".main-header")
 
 
 export default function navMenu() {
@@ -10,15 +8,15 @@ export default function navMenu() {
         $toggleBtn = d.getElementById("toggle-button"),
         $enlacesPrueba = d.querySelectorAll(".enlace")
 
-  // Función para mostrar elementos
-  const mostrarElemento = (elemento, clase) => {
+  // Función para agregar clase
+  const agregaClase = (elemento, clase) => {
     if (!elemento.classList.contains(clase)) {
       elemento.classList.add(clase)
     }
   }
 
-  // Función para ocultar elementos
-  const ocultarElemento = (elemento, clase) => {
+  // Función para quitar clase
+  const quitaClase = (elemento, clase) => {
     if (elemento.classList.contains(clase)) {
       elemento.classList.remove(clase)
     }
@@ -27,9 +25,9 @@ export default function navMenu() {
   // Función que dispara el ocultar o mostrar menú
   const controlaToggle = () => {
     if ($toggleBtn.checked === true) {
-      mostrarElemento($menu, "show-menu");
+      agregaClase($menu, "show-menu");
     } else {
-      ocultarElemento($menu, "show-menu")
+      quitaClase($menu, "show-menu")
     }
   }
 
@@ -50,49 +48,44 @@ export default function navMenu() {
   })
 
 
+
+  // COMPORTAMIENTO BOTÓN SCROLL TO TOP
+  // =========================================================================
   const ocultarBtn = () => {
-    setTimeout(() => {
-      $btnScrollToTop.style.opacity = 0
-      $btnScrollToTop.style.display = "none" 
-    }, 5000)
+    agregaClase($btnScrollToTop, "non-visible")
   }
 
+  const mostrarBtn = () => {
+    quitaClase($btnScrollToTop, "non-visible")
+  }
+
+  
+  // Al hacer scroll
   w.onscroll = () => {
-    // console.log(w.scrollY);
-    // const maxScrollable = d.documentElement.scrollHeight - w.innerHeight    // Máximo scrolleable de la página
-    if (w.scrollY >= 500) {
-      // mostrarElemento($btnScrollToTop, "visible")
-      // ocultarElemento($btnScrollToTop, "non-visible")
-      $btnScrollToTop.style.opacity = 1
-      $btnScrollToTop.style.display = "initial"
+    let scrollTop = w.pageYOffset || d.documentElement.scrollTop
 
-      // ocultarBtn()
-      clearTimeout(ocultarBtn)
-    } 
-    else {
-      // ocultarElemento($btnScrollToTop, "visible")
-      // mostrarElemento($btnScrollToTop, "non-visible")
-      $btnScrollToTop.style.opacity = 0
-      $btnScrollToTop.style.display = "none" 
-      // ocultarBtn()
-
+    if (scrollTop > 500) {
+      mostrarBtn()
+    } else {
+      ocultarBtn()
     }
 
-    ocultarBtn()
+  }
+  
+  let intervalo = setInterval(ocultarBtn, 5000)
+  
+  const play = () => { intervalo = setInterval(ocultarBtn, 3000) },
+        stop = () => { clearInterval(intervalo) }
+
+  $btnScrollToTop.onclick = () => {
+    w.scrollTo({
+      behavior: "smooth",
+      top: 0
+    })
   }
 
+  $btnScrollToTop.addEventListener("mouseover", stop)
+  $btnScrollToTop.addEventListener("mouseleave", play)
 
-  // $btnScrollToTop.addEventListener("hover", () => {
-  //   console.log('hice hover');
-  // })
-  $btnScrollToTop.onmouseenter = () => {
-    // alert('entre al botón')
-    clearTimeout(ocultarBtn)
-  }
-
-  $btnScrollToTop.onmouseleave = () => {
-    // alert('salí del botón')
-    ocultarBtn()
-  }
 
 }
