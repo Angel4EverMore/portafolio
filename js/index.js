110bf0e3e7bbbb1634eef8f8f718b40a2d2fd2b8
Angel4EverMore <jangelmore@gmail.com>
2022-02-22 14:58:36 -0600

    se agrega div en carrousel y se agrega carrousel.js a index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
+// IMPORTS
+import animaCarrousel from "./carrousel.js";
 import navMenu from "./menu.js";
 
 
+// EJECUCIÓN
 document.addEventListener("DOMContentLoaded", event => {
   navMenu()
+  animaCarrousel("slider-portafolio")
 })
